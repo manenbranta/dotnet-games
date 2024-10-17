a729303ba63eea85b74755326baa8f0878bd947d
Chloe <chloe.martins@etec.sp.gov.br>
2024-10-17 11:17:20 -0300

    Add Init() function for cleaner code

--- mauro/src/Program.cs
+++ mauro/src/Program.cs
@@ -12,11 +12,37 @@ class Program
         Player plr = new Player(1,1);
         Wall wall = new Wall(1,2,25,5);
 
-        Game.Instance.AddObject(plr);
+        Init([plr, wall]);
+
         while (gameIsRunning)
         {
             Game.Instance.Update();
             Game.Instance.Render(screen.Width,screen.Height);
+        }
+    }
+
+    static void Init(GameObject[] obj)
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        Console.SetWindowSize(screen.Width, screen.Height);
+
+        if (OperatingSystem.IsWindows())
+        {
+            /*
+            * No Windows, o BufferSize pode ser maior que o WindowSize, 
+            * o que faz uma scrollbar horizontal aparecer. Fazer isso previne esse bug.
+            */
+            Console.SetBufferSize(screen.Width, screen.Height);
+        }
+
+        // O jogo deve usar UTF8, pois usa caracteres fora do set ASCII
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        foreach (var o in obj)
+        {
+            Game.Instance.AddObject(o);
         }
     }
 }
