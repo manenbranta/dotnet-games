164f3fed39cd183e27b2426676992a41c45c4f1a
Chloe <chloe.martins@etec.sp.gov.br>
2024-10-18 10:45:49 -0300

    Restructure forca

--- forca/src/Forca.cs
+++ forca/src/Forca.cs
@@ -4,37 +4,41 @@ namespace Forca
     using System.Threading;
     using System.Globalization;
     using System.Collections.Generic;
-    class Forca
+    class Game
     {
+        static List<string> listaPalavras;
+        static Dictionary<string,string> dicas;
+        static int erros = 0;
+
         static void Main()
         {
-            string[] listaPalavras = {
-                "amarelo",
+            // Todas as palavras disponíveis para o jogo escolher aleatoriamente 
+            listaPalavras = new List<string>() {
+                /*"amarelo",
                 "miserável",
                 "caatinga",
                 "transeunte",
-                "quarentena",
+                "quarentena",*/
                 "pokémon"
             };
 
-            Random rng = new Random();
+            // Hashmap com as dicas para cada palavra
+            dicas = new Dictionary<string, string>
+            {
+                { "amarelo", "Cor do maior astro próximo da terra" },
+                { "miserável", "Antônimo da palavra pobre" },
+                { "caatinga", "Bioma brasileiro" },
+                { "transeunte", "Aquele ou aquilo em constante movimento" },
+                { "quarentena", "Sinônimo de isolação" },
+                { "pokemon", "Jogo da Nintendo que têm monstrinhos" }
+            };
 
             // Palavra escolhida pro jogo de forca
-            string palavra;
+            Palavra wrd = new Palavra(listaPalavras.ToArray(),dicas);
+            // Versão string da palavra
+            string palavra = wrd.Get();
+            // Uma lista com todas as letras que o usuário já tentou.
             List<char> letrasRepetidas = new List<char>();
-
-            int erros = 0;
-
-            //Hashmap com as dicas para cada palavra
-            Dictionary<string,string> dicas = new Dictionary<string,string>();
-            dicas.Add("amarelo", "Cor do maior astro próximo da terra");
-            dicas.Add("miserável", "Antônimo da palavra pobre");
-            dicas.Add("caatinga", "Bioma brasileiro");
-            dicas.Add("transeunte", "Aquele ou aquilo em constante movimento");
-            dicas.Add("quarentena", "Sinônimo de isolação");
-            dicas.Add("pokemon", "Jogo da Nintendo que têm monstrinhos");
-
-            palavra = listaPalavras[rng.Next(0,listaPalavras.Length)];
 
             string cesar = new string('_',palavra.Length);
 
@@ -49,41 +53,37 @@ namespace Forca
                 Window.ClearPos(Console.WindowWidth/2-40,Console.WindowHeight/2+2,80);
                 Window.WriteCenter(cesar,0);
                 Window.WriteCenter("Escreva uma letra: ",1);
-                string read = Console.ReadLine().Trim(' ');
-                char letra = read.ToCharArray()[0];
-                if (read == "dica")
+                char[] read = Console.ReadLine().Trim(' ').ToCharArray();
+                char letra = read[0];
+                if (read.ToString() == "dica")
                 {
-                    Window.WriteCenter(dicas[palavra],2);
+                    Window.WriteCenter(wrd.GetDica(palavra),2);
                 }
                 else if (letrasRepetidas.Contains(letra))
                 {
                     Window.WriteCenter("Você já escolheu essa letra!",2);
                     Thread.Sleep(500);
                 }
-                else
+                else if (wrd.HasChar(letra))
                 {
-                    bool found = false;
                     char[] cesarArray = cesar.ToCharArray();
                     letrasRepetidas.Add(letra);
 
-                    for (int i = 0; i<palavra.Length; i++)
+                    for (int i = 0; i < palavra.Length; i++)
                     {
                         if (string.Compare(palavra[i].ToString(), letra.ToString(), CultureInfo.CurrentCulture, CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreCase) == 0)
                         {
                             cesarArray[i] = palavra[i];
-                            found = true;
                         }
                     }
-
                     cesar = new string(cesarArray);
-
-                    if (!found)
-                    {
-                        erros++;
-                        Window.WriteCenter("Não há essa letra na palavra escolhida!", 2);
-                        Window.WriteCenter($"ERROS: {erros}", Console.BufferHeight/2-3);
-                        Thread.Sleep(500);
-                    }
+                }
+                else
+                {
+                    erros++;
+                    Window.WriteCenter("Não há essa letra na palavra escolhida!", 2);
+                    Window.WriteCenter($"ERROS: {erros}", Console.BufferHeight/2-3);
+                    Thread.Sleep(500);
                 }
             } while(cesar != palavra);
 
@@ -92,9 +92,93 @@ namespace Forca
             Console.Clear();
         }
 
-        static string fmt(string str) 
+        /*static void Init(palavras)
         {
-            return str[0].ToString().ToUpper() + str.Substring(1).ToLower();
+
+        }*/
+    }
+
+    class Palavra 
+    {
+        string _palavra;
+        string[] _listaPalavras;
+
+        Dictionary<string,string> _dicas;
+
+        Random _rng;
+
+        public Palavra(string[] list, Dictionary<string,string> dicas)
+        {
+            _rng = new Random();
+            _listaPalavras = list;
+            _palavra = _listaPalavras[_rng.Next(0,_listaPalavras.Length)];
+
+            /* 
+            * Inicializar o hashmap assim faz com que as keys ignorem a capitalização das palavras.
+            * Então, por exemplo `_dicas["CaAtInGa"]`, `_dicas["CAATINGA"]` e `_dicas["caatinga"]`
+            * todos retornam a mesma coisa.
+            */
+            _dicas = new Dictionary<string, string>(dicas, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public bool HasChar(char character)
+        {
+            foreach (char s in _palavra)
+            {
+                if (CharsEqual(s, character))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Essa é uma função que retorna uma dica para o usuário
+        /// que irá ajudá-lo a descobrir a palavra atual.
+        /// </summary>
+        /// <param name="word">A palavra do qual o usuário quer a dica.</param>
+        /// <returns>A dica, em uma string.</returns>
+        public string GetDica(string word)
+        {
+            return _dicas[word];
+        }
+
+        /// <summary>
+        /// Um getter é usado aqui pois a palavra não pode ser alterada, a não ser em seu construtor.
+        /// </summary>
+        /// <returns>A palavra atual.</returns>
+        public string Get() 
+        {
+            return _palavra;
+        }
+
+        /// <summary>
+        /// Essa função é usada em lugares como a tela de derrota, onde é mostrado na tela a palavra que
+        /// foi sorteada na rodada onde o jogador perdeu.
+        /// </summary>
+        /// <returns>A palavra atual, formatada</returns>
+        /// <example>
+        /// <code>
+        /// Palavra wrd = new Palavra(["CaAtInGa"],{{"CaAtInGa","Bioma brasileiro"}});
+        /// string palavra = wrd.Get(); // Digamos que a palavra escolhida foi CaAtInGa.
+        /// palavra = wrd.GetFormatted();
+        /// System.Console.WriteLine(palavra); // -> Caatinga
+        /// </code>
+        /// </example>
+        public string GetFormatted() 
+        {
+            return _palavra[0].ToString().ToUpper() + _palavra.Substring(1).ToLower();
+        }
+
+        bool CharsEqual(char char1, char char2)
+        {
+            if (string.Compare(char1.ToString(), char2.ToString(), CultureInfo.CurrentCulture, CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreCase) == 0)
+            {
+                return true;
+            }
+            
+            return false;
         }
     }
 }
